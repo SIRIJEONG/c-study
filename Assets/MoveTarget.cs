22dbc2d580f4a456577afe73e6584715508f5a39
SIRI JEOUNG <100827304+SIRIJEONG@users.noreply.github.com>
2023-04-07 13:27:07 +0900

    study

--- Assets/MoveTarget.cs
+++ Assets/MoveTarget.cs
@@ -14,11 +14,23 @@ public class MoveTarget : MonoBehaviour
         //    Vector3.MoveTowards(transform.position
         //                               , target, 1f); //매개변수는 (현재위치,목표위치,속도)로 구성 
 
-        //2.SmoothDamp
-        Vector3 velo = Vector3.zero;
+        ////2.SmoothDamp
+        //Vector3 velo = Vector3.zero;
 
+        //transform.position =
+        //    Vector3.SmoothDamp(transform.position
+        //                                , target, ref velo, 0.1f); //매개변수는 (현재위치,목표위치,참조속도,속도) //ref=참조접근(실시간으로 바뀌는 값 적용 가능)
+
+
+        ////3/Lerp(선형 보간)
+        //transform.position =
+        //    Vector3.Lerp(transform.position
+        //                                , target, 0.05f); 
+
+
+        //4.SLerp(구면 선형 분감)
         transform.position =
-            Vector3.SmoothDamp(transform.position
-                                        , target, ref velo, 0.1f); //매개변수는 (현재위치,목표위치,참조속도,속도) //ref=참조접근(실시간으로 바뀌는 값 적용 가능)
+            Vector3.Slerp(transform.position
+                                        , target, 0.1f); //포물선 이동 
     }
 }
